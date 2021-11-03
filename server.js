37622debe9f49e172b8883040555dc66864eda80
sergiiglad <sergii.gladchenko@gmail.com>
2021-11-03 18:19:13 +0200

    body

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const secret1 = 'projects/1019145220404/secrets/mysql_password/versions/latest'
 
+var body = 'Hello from App Engine!'
 
 // Imports the Secret Manager library
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
@@ -20,7 +21,7 @@ async function accessSecretVersion(name) {
 
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!',accessSecretVersion(secret1));
+  res.send(body.concat(accessSecretVersion(secret1)));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
