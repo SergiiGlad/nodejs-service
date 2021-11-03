f97cc67051dc06c20c283e6c19e77188b4e0e604
Sergii Gladchenko <sgladc@softserveinc.com>
2021-11-03 17:48:39 +0200

    add secret

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const secret1 = 'projects/427943077440/secrets/controller-app-db-username-dev/versions/latest'
+
+
+// Imports the Secret Manager library
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+// Instantiates a client
+const client = new SecretManagerServiceClient();
+
+async function accessSecretVersion(name) {
+  const [version] = await client.accessSecretVersion({
+    name: name,
+  });
+
+  // Extract the payload as a string.
+  return version.payload.data.toString();
+}
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.send('Hello from App Engine!',accessSecretVersion(secret1));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
