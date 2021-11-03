6cb0c3bc9d622b9d0fcbc060bf6314c6352f34ab
sergiiglad <sergii.gladchenko@gmail.com>
2021-11-03 18:03:38 +0200

    secret

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ async function accessSecretVersion(name) {
 
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  res.send('Hello from App Engine!',accessSecretVersion(secret1));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
