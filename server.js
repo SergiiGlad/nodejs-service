d2f4a7ef5db62d3413b5385617ca5bcc05ee417d
sergiiglad <sergii.gladchenko@gmail.com>
2021-11-03 18:26:15 +0200

    secret1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 // Instantiates a client
 const client = new SecretManagerServiceClient();
 
-function accessSecretVersion(name) {
+async function accessSecretVersion(name) {
   const [version] = await client.accessSecretVersion({
     name: name,
   });
@@ -21,7 +21,7 @@ function accessSecretVersion(name) {
 
 
 app.get('/', (req, res) => {
-  res.send(body.concat(accessSecretVersion(secret1)));
+  res.send(accessSecretVersion(secret1));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
