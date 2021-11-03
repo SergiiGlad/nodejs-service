3bfbbc5b8249dad025ee76ee7fa37a01a2d2db5c
Sergii Gladchenko <sgladc@softserveinc.com>
2021-11-03 18:23:22 +0200

    1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
